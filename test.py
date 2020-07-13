997cd6d308a4a1061c84e9262eaa6c35c20a9f66
Ying Nam Lee (yinglee) <yinglee@cisco.com>
2020-07-13 09:52:13 +0800

    commit

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-a = 10
-b = 20
-c = a + b
-print (c)
 
-print('HelloWorld')
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
